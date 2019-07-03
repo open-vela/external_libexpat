1de794e3addf562edab4704b6ac037c7b1f06913
liujinye <liujinye@xiaomi.com>
2019-07-03 22:53:12 +0800

    Merge branch 'MohammedKhajapasha-master' (#264)

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -2080,6 +2080,9 @@ XML_GetBuffer(XML_Parser parser, int len)
       parser->m_bufferLim = newBuf + bufferSize;
 #ifdef XML_CONTEXT_BYTES
       if (parser->m_bufferPtr) {
+        int keep = (int)EXPAT_SAFE_PTR_DIFF(parser->m_bufferPtr, parser->m_buffer);
+        if (keep > XML_CONTEXT_BYTES)
+          keep = XML_CONTEXT_BYTES;
         memcpy(newBuf, &parser->m_bufferPtr[-keep],
                EXPAT_SAFE_PTR_DIFF(parser->m_bufferEnd, parser->m_bufferPtr) + keep);
         FREE(parser, parser->m_buffer);
@@ -4976,22 +4979,18 @@ doProlog(XML_Parser parser,
     case XML_ROLE_GROUP_OPEN:
       if (parser->m_prologState.level >= parser->m_groupSize) {
         if (parser->m_groupSize) {
-          {
-            char * const new_connector = (char *)REALLOC(parser,
-                parser->m_groupConnector, parser->m_groupSize *= 2);
-            if (new_connector == NULL) {
-                parser->m_groupSize /= 2;
-                return XML_ERROR_NO_MEMORY;
-            }
-            parser->m_groupConnector = new_connector;
+          char *temp = (char *)REALLOC(parser, parser->m_groupConnector, parser->m_groupSize *= 2);
+          if (temp == NULL) {
+            parser->m_groupSize /= 2;
+            return XML_ERROR_NO_MEMORY;
           }
-
+          parser->m_groupConnector = temp;
           if (dtd->scaffIndex) {
-            int * const new_scaff_index = (int *)REALLOC(parser,
-                dtd->scaffIndex, parser->m_groupSize * sizeof(int));
-            if (new_scaff_index == NULL)
+            int *temp = (int *)REALLOC(parser, dtd->scaffIndex,
+                          parser->m_groupSize * sizeof(int));
+            if (temp == NULL)
               return XML_ERROR_NO_MEMORY;
-            dtd->scaffIndex = new_scaff_index;
+            dtd->scaffIndex = temp;
           }
         }
         else {
